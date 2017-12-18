bbf91638d9a5cae6a01756f6e9a338b595d5a213
dolgeor <dolgeor@yandex.ru>
2017-12-18 15:23:41 +0200

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+ // templateUrl: './app.component.html',
+  template: `hello`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
