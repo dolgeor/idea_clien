e914ab63ab4aba3ab17d8a712f780a2002904d74
dolgeor <dolgeor@yandex.ru>
2017-12-19 12:04:52 +0200

    1

--- Server.js
+++ Server.js
@@ -15,19 +15,71 @@
 // console.log('test server js')
 
 
-var express     = require('express');
-var herokuProxy = require('heroku-proxy');
-var app         = express();
-const path = require('path');
-// ...set up heroku-bouncer 
+// var express     = require('express');
+// var herokuProxy = require('heroku-proxy');
+// var app         = express();
+// const path = require('path');
+// // ...set up heroku-bouncer 
  
-app.use(herokuProxy({
-    hostname: 'isd-ideas-back.herokuapp.com/ideas/',
-    port    :  process.env.PORT || 8080,
-    prefix  : 'heroku-api',
-    protocol: 'http'
-  }));
+// app.use(herokuProxy({
+//     hostname: 'isd-ideas-back.herokuapp.com/ideas/',
+//     port    :  process.env.PORT || 8080,
+//     prefix  : 'heroku-api',
+//     protocol: 'http'
+//   }));
 
-  app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+//   app.get('/*',function(req,res){
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// })
+
+
+
+var http = require('http')
+
+var url = require('url')
+
+var server = http.createServer(function(request, response) {
+
+    console.log(request.url)
+
+    var ph = url.parse(request.url)
+
+    var options = {
+
+        port: ph.port,
+
+        hostname: ph.hostname,
+
+        method: request.method,
+
+        path: ph.path,
+
+        headers: request.headers
+
+    }
+
+    var proxyRequest = http.request(options)
+
+    proxyRequest.on('response', function(proxyResponse) {
+
+        proxyResponse.on('data', function(chunk) {
+
+            response.write(chunk, 'binary')
+
+        })
+
+        proxyResponse.on('end', function() { response.end() })
+
+        response.writeHead(proxyResponse.statusCode, proxyResponse.headers)
+
+    })
+
+    request.on('data', function(chunk) {
+
+        proxyRequest.write(chunk, 'binary')
+
+    })
+
+    request.on('end', function() { proxyRequest.end() })
+
+}).listen(8080)
