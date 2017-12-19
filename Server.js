925a299c9d93c499f6d741fbb38f0dc850b8aa29
dolgeor <dolgeor@yandex.ru>
2017-12-19 12:09:19 +0200

    1

--- Server.js
+++ Server.js
@@ -1,18 +1,18 @@
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-// // Run the app by serving the static files
-// // in the dist directory
-// app.use(express.static(__dirname + '/dist'));
-// // Start the app by listening on the default
-// // Heroku port
-// app.listen(process.env.PORT || 8080);
+const express = require('express');
+const app = express();
+const path = require('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 
-// app.get('/*',function(req,res){
-//     res.sendFile(path.join(__dirname + '/dist/index.html'));
-// })
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
 
-// console.log('test server js')
+console.log('test server js')
 
 
 // var express     = require('express');
@@ -32,54 +32,3 @@
 //     res.sendFile(path.join(__dirname + '/dist/index.html'));
 // })
 
-
-
-var http = require('http')
-
-var url = require('url')
-
-var server = http.createServer(function(request, response) {
-
-    console.log(request.url)
-
-    var ph = url.parse(request.url)
-
-    var options = {
-
-        port: ph.port,
-
-        hostname: ph.hostname,
-
-        method: request.method,
-
-        path: ph.path,
-
-        headers: request.headers
-
-    }
-
-    var proxyRequest = http.request(options)
-
-    proxyRequest.on('response', function(proxyResponse) {
-
-        proxyResponse.on('data', function(chunk) {
-
-            response.write(chunk, 'binary')
-
-        })
-
-        proxyResponse.on('end', function() { response.end() })
-
-        response.writeHead(proxyResponse.statusCode, proxyResponse.headers)
-
-    })
-
-    request.on('data', function(chunk) {
-
-        proxyRequest.write(chunk, 'binary')
-
-    })
-
-    request.on('end', function() { proxyRequest.end() })
-
-}).listen(8080)
