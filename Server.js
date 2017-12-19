a9e5dc1930467e52f7454bc4668a55e6ff131e01
dolgeor <dolgeor@yandex.ru>
2017-12-19 10:54:21 +0200

    3

--- Server.js
+++ Server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-var herokuProxy = require('heroku-proxy');
 const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-//////////////////////////////////app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -12,12 +11,5 @@ app.listen(process.env.PORT || 8080);
 app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
-
-app.use(herokuProxy({
-    hostname: 'isd-ideas-back.herokuapp.com/ideas',
-    port    : 8080,
-    prefix  : 'heroku-api',
-    protocol: 'http'
-  }));
 
 console.log('test server js')
